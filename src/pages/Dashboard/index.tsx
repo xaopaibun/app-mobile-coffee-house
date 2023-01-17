9f963428a3bceb379e7f51650f1770d64a04e6df
xaopaibun <vanquy33338888@gmail.com>
2023-01-17 16:59:09 +0700

    update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -156,6 +156,7 @@ const Dashboard: FC = () => {
   //   };
   // }, []);
 
+  console.log('abcd');
   return (
     <DashboardStyled>
       <HeaderDashboard title="Dashboard" className="header" />
